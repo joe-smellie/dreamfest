8ae360a07b82540458042b2efb41ee36a1d9d5ef
Joe Smellie <joesmellie1@gmail.com>
2024-05-29 10:56:32 +1200

    Step 1 complete

--- server/db/index.ts
+++ server/db/index.ts
@@ -3,6 +3,7 @@ import knex from 'knex'
 import type { Location, LocationData } from '../../models/Location.ts'
 import type { Event, EventWithLocation, EventData } from '../../models/Event.ts'
 
+
 type Environment = 'production' | 'test' | 'development'
 
 const environment = (process.env.NODE_ENV || 'development') as Environment
@@ -10,6 +11,10 @@ const config = knexFile[environment]
 export const connection = knex(config)
 
 export async function getAllLocations() {
-  const locations: unknown[] = [] // TODO: replace this with your knex query
+  const locations: unknown[] = await connection('locations')
+    .select(
+      '*'
+    ) 
+    // TODO: replace this with your knex query
   return locations as Location[]
 }
--- server/routes/locations.ts
+++ server/routes/locations.ts
@@ -8,20 +8,21 @@ const router = express.Router()
 router.get('/', async (req, res, next) => {
   try {
     // TODO: Replace this with all of the locations in the database
-    const locations = [
-      {
-        id: 1,
-        name: 'TangleStage',
-        description:
-          'Not the biggest stage, but perhaps the most hip. Not the biggest stage, but perhaps the most hip. Not the biggest stage, but perhaps the most hip.',
-      },
-      {
-        id: 2,
-        name: 'Yella Yurt',
-        description:
-          "It's a freakin' yurt! Get in here! It's a freakin' yurt! Get in here! It's a freakin' yurt! Get in here! It's a freakin' yurt! Get in here!",
-      },
-    ]
+    const locations = await db.getAllLocations()
+    //[
+    //   {
+    //     id: 1,
+    //     name: 'TangleStage',
+    //     description:
+    //       'Not the biggest stage, but perhaps the most hip. Not the biggest stage, but perhaps the most hip. Not the biggest stage, but perhaps the most hip.',
+    //   },
+    //   {
+    //     id: 2,
+    //     name: 'Yella Yurt',
+    //     description:
+    //       "It's a freakin' yurt! Get in here! It's a freakin' yurt! Get in here! It's a freakin' yurt! Get in here! It's a freakin' yurt! Get in here!",
+    //   },
+    // ]
 
     res.json({ locations })
   } catch (e) {
